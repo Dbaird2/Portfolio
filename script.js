988852b111cd9113026d963082d9f863aed4b5de
Dbaird2 <dasonbaird25@gmail.com>
2026-01-04 20:49:19 -0800

    adding js for navbar color changing, redid navbar

---
+++ script.js
@@ -0,0 +1,23 @@
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".sidenav a");
+console.log(sections)
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      const link = document.querySelector(
+        `.sidenav a[href="#${entry.target.id}"]`
+      );
+
+      if (entry.isIntersecting) {
+        link.classList.add("active");
+      } else {
+        link.classList.remove("active"); 
+      }
+    });
+  },
+  { threshold: 0.1 }
+);
+
+sections.forEach((section) => {
+  observer.observe(section);
+});
