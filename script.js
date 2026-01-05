265d4f7bf2c4ded2233203ede09ccdbbc8d61fbb
Dbaird2 <dasonbaird25@gmail.com>
2026-01-05 14:35:36 -0800

    added background color and animation changes

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 const sections = document.querySelectorAll("section");
 const navLinks = document.querySelectorAll(".sidenav a");
-console.log(sections)
+
+console.log(sections);
 const observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
       const link = document.querySelector(
         `.sidenav a[href="#${entry.target.id}"]`
       );
+      const card = document.querySelector(
+        `.all-info section[id="${entry.target.id}"]`
+      );
 
       if (entry.isIntersecting) {
         link.classList.add("active");
+        card.classList.add("active-card");
+        console.log(card)
       } else {
-        link.classList.remove("active"); 
+        link.classList.remove("active");
+        card.classList.remove("active-card");
       }
     });
   },
-  { threshold: 0.3 }
+  { threshold: 0.2 }
 );
 
 sections.forEach((section) => {
