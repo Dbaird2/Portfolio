86925e47d05082dfcf6aca09ec117a6aef927a19
dbaird2 <dasonbaird25@gmail.com>
2026-02-25 15:34:02 -0700

    dynamically adding directory from repo instead of manual. Added all my projects.

--- script.js
+++ script.js
@@ -28,6 +28,113 @@
 // sections.forEach((section) => {
 //   observer.observe(section);
 // });
+async function loadCode(file) {
+  const response = await fetch(
+    "https://raw.githubusercontent.com/Dbaird2/" + file,
+  );
+
+  const code = await response.text();
+  document.getElementById("code-block").textContent = code;
+  Prism.highlightAll();
+}
+async function loadRepo(repo) {
+  const response = await fetch(
+    "https://api.github.com/repos/Dbaird2/" +
+      repo +
+      "/git/trees/main?recursive=1",
+  );
+
+  const data = await response.json();
+  console.log(data.tree);
+
+  return data;
+}
+async function createDirectoryStructure(repo) {
+  const data = await loadRepo(repo);
+  const tree = data.tree;
+
+  tree.forEach((item) => {
+    const path_parts = item.path.split("/");
+    if (path_parts[0] === "vendor") {
+      return; // Skip the 'vendor' directory and its contents
+    }
+    const mode = item.mode; // '100644' for files, '040000' for directories
+
+    if (mode === "040000") {
+      // It's a folder
+      let id = path_parts.slice(0, path_parts.length).join("-") + "-sub-list";
+      let prev_id =
+        path_parts.slice(0, path_parts.length - 1).join("-") + "-sub-list";
+      /*
+      console.log(
+        document.querySelector("." + repo + "-sub-list"),
+        id,
+        prev_id,
+        mode,
+        path_parts,
+      );
+      */
+      if (path_parts.length === 1) {
+        // Top Level
+
+        document.querySelector("." + repo + "-sub-list").insertAdjacentHTML(
+          "beforeend",
+          `<li><a
+            class="folder"
+            href="#"
+            onclick="toggleSubList(this)"
+            id="${id}"
+            ><i class="fa-thin fa-angle-down"></i>${path_parts[path_parts.length - 1]}</a
+          >
+          <ul class="${id} sub-list"></ul></li>`,
+        );
+      } else {
+        // console.log("folder", path_parts, prev_id, id);
+
+        document.querySelector("." + prev_id).insertAdjacentHTML(
+          "beforeend",
+          `<li><a
+            class="folder"
+            href="#"
+            onclick="toggleSubList(this)"
+            id="${id}"
+            ><i class="fa-thin fa-angle-down"></i>${path_parts[path_parts.length - 1]}</a
+          >
+          <ul class="${id} sub-list"></ul></li>`,
+        );
+      }
+    } else {
+      // It's a file
+      let id;
+      if (path_parts.length === 1) {
+        id = "." + repo + "-sub-list";
+      } else {
+        id =
+          "." +
+          path_parts.slice(0, path_parts.length - 1).join("-") +
+          "-sub-list";
+      }
+      /* console.log(
+        document.querySelector("." + repo + "-sub-list"),
+        id,
+        mode,
+        path_parts,
+      );
+      */
+      document.querySelector(id).insertAdjacentHTML(
+        "beforeend",
+        `<li>
+              <a
+                class="file"
+                href="#"
+                onclick="loadCode('${repo}/main/${item.path}')"
+                >${item.path}</a
+              >
+            </li>`,
+      );
+    }
+  });
+}
 function loadCard(file) {
   console.log("Loading card:", file);
   fetch(file)
@@ -49,6 +156,14 @@ small_card_files.forEach((file) => loadCard(file));
 
 showCard("intro-small-card");
 sidebarActive(document.getElementById("intro"));
+createDirectoryStructure("seniorproject");
+createDirectoryStructure("blok");
+createDirectoryStructure("Stock-Predictor");
+createDirectoryStructure("ML-group-project");
+createDirectoryStructure("games");
+createDirectoryStructure("senior-mobile");
+createDirectoryStructure("Database-Group-Project");
+createDirectoryStructure("MariaDB-Research-Report");
 
 const side_clicks = document.querySelectorAll(".contents");
 
